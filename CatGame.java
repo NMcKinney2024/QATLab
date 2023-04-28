52315cc2592fdae9477b9807d189b0647cca4612
McKinney <nmckinney@friendsbalt.org>
2023-04-28 13:00:21 -0400

    All methods finished, untested, I'm hungry

--- CatGame.java
+++ CatGame.java
@@ -1,39 +1,76 @@
 import edu.princeton.cs.algs4.EdgeWeightedGraph;
+import edu.princeton.cs.algs4.DijkstraUndirectedSP;
+import edu.princeton.cs.algs4.Edge;
 
 public class CatGame {
-    private EdgeWeightedGraph g;
+    int bs;
+    int[] catPos = {0, 0};
+    boolean[] markedTiles;
+    EdgeWeightedGraph g;
+
     CatGame(int n) {
         g = new EdgeWeightedGraph((n*n)+1);
 
-        for (int bread = 0; bread < n; bread++) {
-            for (int pita = 0; pita < n; pita++) {
+        //Create all edges.
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
                 //Current node
-                int v = bread*n + pita;
+                int v = row*n + col;
                 //Connect forward
-                if (pita != n-1) g.addEdge(new CatEdge(v, v+1));
+                if (col != n-1) g.addEdge(new CatEdge(v, v+1));
                 //Connect down
-                if (bread != n-1) g.addEdge(new CatEdge(v, (bread+1)*n + pita));
+                if (row != n-1) g.addEdge(new CatEdge(v, (row+1)*n + col));
                 //If even row connect down and to the left, if odd connect down and to the right
-                if (bread % 2 == 0) {
-                    if (pita != 0) g.addEdge(new CatEdge(v, (bread+1)*n + pita-1));
+                if (row % 2 == 0) {
+                    if (col != 0) g.addEdge(new CatEdge(v, (row+1)*n + col-1));
                 } else {
-                    if (pita != n-1) g.addEdge(new CatEdge(v, (bread+1)*n + pita+1));
+                    if (col != n-1) g.addEdge(new CatEdge(v, (row+1)*n + col+1));
                 }
                 //Connect all edges to freedom hexagon
-                if (bread == 0 || bread == n-1 || pita == 0 || pita == n-1) g.addEdge(new CatEdge(v, (n*n)));
+                if (row == 0 || row == n-1 || col == 0 || col == n-1) g.addEdge(new CatEdge(v, (n*n)));
             }
-        } 
+        }
+
+        //Start cat in the middle.
+        catPos[0] = n/2;
+        catPos[1] = n/2;
+
+        bs = n;
+        markedTiles = new boolean[(n*n)+1];
     }
 
-    void markTile(int row, int col) {}
+    void markTile(int row, int col) {
+        int v = row*bs + col;
+        markedTiles[v] = true;
+        int cat = catPos[0]*bs + catPos[1];
+        for(Edge e : g.adj(v)) {
+            CatEdge ce = (CatEdge) e;
+            ce.setWeight(Double.POSITIVE_INFINITY);
+        }
+        DijkstraUndirectedSP sp = new DijkstraUndirectedSP(g, cat);
+        CatEdge next = (CatEdge) sp.pathTo(bs*bs).iterator().next();
+        int nextSquare = next.other(cat);
+        catPos[0] = nextSquare/bs;
+        catPos[1] = nextSquare%bs;
+    }
 
-    boolean marked(int row, int col) {}
+    boolean marked(int row, int col) {
+        return markedTiles[row*bs + col];
+    }
 
-    int[] getCatTile() {}
+    int[] getCatTile() {
+        return catPos;
+    }
 
-    boolean catHasEscaped() {}
+    boolean catHasEscaped() {
+        if (catPos[0]*bs+catPos[1] == bs*bs) return true;
+        else return false;
+    }
 
-    boolean catIsTrapped() {}
-
-    
+    boolean catIsTrapped() {
+        int s = catPos[0]*bs + catPos[1];
+        DijkstraUndirectedSP sp = new DijkstraUndirectedSP(g, s);
+        if (sp.distTo(bs*bs) == Double.POSITIVE_INFINITY) return true;
+        else return false;
+    }
 }
